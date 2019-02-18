d9b21a408a06f1e97c04fbce91949163bcc7617d
Michael Pajeau <16153695+mpajeau@users.noreply.github.com>
2019-02-17 19:36:28 -0600

    First commit.
    importPuzzle() always throws an IOException.

---
+++ README.md
@@ -0,0 +1,4 @@
+This project contains a java program that can read a text file containing a word search grid and a list of search words and then solve the puzzle by reporting where each search word occurs within the grid.
+
+Usage:
+
---
+++ src/main/java/net/pajeau/WordSearch.java
@@ -0,0 +1,16 @@
+package net.pajeau;
+
+import java.io.*;
+
+public class WordSearch {
+
+    // This method imports a puzzle from a passed-in Reader (e.g., string or file).  The puzzle
+    // should contain a list of comma-separated search words on the first line.  Each search word
+    // must be at least two letters long.  Each subsequent line of the file should contain 
+    // comma-separated letters making up one row of the grid to be searched.  The grid must
+    // be square.
+    public void importPuzzle(Reader thePuzzleReader) throws IOException
+    {
+        throw new IOException("No puzzle data was provided.");
+    }
+}
